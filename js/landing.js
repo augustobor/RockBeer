e6f580488607095e0bc2932eb12434ff12876a90
Augusto <augus_borromeo@hotmail.com>
2022-04-01 23:59:47 -0300

    rock beer designed with vue

--- js/landing.js
+++ js/landing.js
@@ -1,15 +1,3 @@
-datetime = document.getElementById("landing--card-day")
-
-const date = new Date()
-const now = new Date()
-now.setDate(date.getDate() - 1)
-
-datetime.min = now.toLocaleDateString()
-
-const week = new Date()
-week.setDate(date.getDate() + 7)
-datetime.max = week.toLocaleDateString()
-
 
 const productsListCombo = [
 
@@ -20,21 +8,3 @@ const productsListCombo = [
 
 ]
 
-
-const datalist = document.getElementById('products')
-
-productsListCombo.forEach(item => {
-
-    product = document.createElement('option')
-    product.value = item.name
-
-    datalist.appendChild(product)
-
-})
-
-const submit = document.getElementById('landing--card-button')
-
-submit.onclick = () => {
-    const success = document.createElement('p')
-    success.innerHTML = "Reserved sucessfully!"
-}
